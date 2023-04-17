8685328adc5c679676fa553478a39d011ee32719
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-04-17 18:13:18 +0200

    añadido UML y ya funciona correctamente

--- gasolinera.py
+++ gasolinera.py
@@ -22,8 +22,6 @@ class Coche(threading.Thread):
         self.gasolinera.ocupar_surtidor(self)
         self.gasolinera.pagar(self)
         self.gasolinera.salir(self)
-        self.tiempo = time.time() - self.tiempo
-        print(f"El coche {self.id} ha tardado {self.tiempo} segundos en repostar")
 
 class Gasolinera:
     def __init__(self, surtidores):
@@ -33,38 +31,40 @@ class Gasolinera:
         self.cola = []
         self.cola_pago = []
         self.cola_salida = []
-        self.tiempo = 0
-        self.tiempo_medio = 0
         self.coche = 0
 
     def llega(self, coche):
         print(f"El coche {coche.id} ha llegado a la gasolinera")
-        time.sleep(random.randint(1, 15)/100.0)
 
     def ocupar_surtidor(self, coche):
-        print("El coche %d se ha puesto en el surtidor" % coche.id)
+        print(f"El coche {coche.id} se ha puesto en la cola de surtidores")
         coche.surtidor = self.surtidores_libres
         self.surtidores_libres -= 1
         self.surtidores_ocupados += 1
-        time.sleep(random.randint(5, 10)/100.0)
         self.surtidores_libres += 1
         self.surtidores_ocupados -= 1
+        time.sleep(random.randint(5,10))
         print(f"El coche {coche.id} ha terminado de repostar")
 
     def pagar(self, coche):
         print(f"El coche {coche.id} se ha puesto en la cola de pago")
         self.cola_pago.append(coche)
         time.sleep(3)
-        print("El coche %d ha terminado de pagar" % coche.id)
+        print(f"El coche {coche.id} ha pagado")
 
     def salir(self, coche):
-        print("El coche %d se ha puesto en la cola de salida" % coche.id)
+        print(f"El coche {coche.id} se ha puesto en la cola de salida")
         self.cola_salida.append(coche)
         time.sleep(1)
-        print("El coche %d ha salido de la gasolinera" % coche.id)
 
 
 def iniciar():
     gasolinera = Gasolinera(1)
+    tiempo_inicial = time.time()
     for i in range(50):
         Coche(i, gasolinera)
+    tiempo_final = time.time()
+    tiempo_ejecucion = tiempo_final - tiempo_inicial
+    time.sleep(30)
+    tiempo_final = (tiempo_ejecucion*20000)
+    print(f"El tiempo que han tardado todos los coches en repostar ha sido de {tiempo_final} minutos")
